833b521387a0754bff71552469c3ab9b2e3485bf
laiconglin <laiconglin3@gmail.com>
2017-08-02 01:16:40 +0800

    update

---
+++ tests/DependencyAndDataProviderComboTest.php
@@ -0,0 +1,31 @@
+<?php
+use PHPUnit\Framework\TestCase;
+class DependencyAndDataProviderComboTest extends TestCase
+{
+
+	public function testProducerFirst()
+	{
+		$this->assertTrue(true);
+		return 'first';
+	}
+	public function testProducerSecond()
+	{
+		$this->assertTrue(true);
+		return 'second';
+	}
+	public function provider()
+	{
+		return [['provider1']];
+	}
+	/**
+	 * @depends testProducerFirst
+	 * @depends testProducerSecond
+	 * @dataProvider provider
+	 */
+	public function testConsumer()
+	{
+		$this->assertEquals(
+			['provider1', 'first', 'second'],
+			func_get_args()
+		); }
+}
